e6a5c7cf3d9636fa85f07c8f12dfa8736ecdce2b
William <goiswilliam194@gmail.com>
2023-10-26 14:51:37 -0300

    change profile image pic

--- src/services/changeOrderToImageProfilePic.ts
+++ src/services/changeOrderToImageProfilePic.ts
@@ -4,12 +4,34 @@ export async function changeOrderToImageProfilePic(
   imageId: number,
   order: number,
 ) {
-  await prismaClient.playdate_auth_profilepic.update({
+  const result = await prismaClient.playdate_auth_profilepic.update({
     where: {
       id: imageId,
     },
     data: {
       order,
+    },
+    select: {
+      playdate_auth_profile: {
+        select: {
+          id: true,
+        },
+      },
+    },
+  })
+
+  await prismaClient.playdate_auth_profilepic.updateMany({
+    where: {
+      profile_id: Number(result.playdate_auth_profile.id),
+      order,
+      NOT: {
+        id: imageId,
+      },
+    },
+    data: {
+      order: {
+        increment: 1,
+      },
     },
   })
 }
