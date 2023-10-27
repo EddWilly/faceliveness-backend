a0874bc9d9806165459939e6f80afa3aab462400
William <goiswilliam194@gmail.com>
2023-10-27 12:34:02 -0300

    fix: configure aws rekognition client

--- src/controllers/createRekognitionClientSession.ts
+++ src/controllers/createRekognitionClientSession.ts
@@ -7,23 +7,31 @@ export async function createRekognitionClientSessionController(
   req: Request,
   reply: Response,
 ) {
-  const { sessionId } = req.query as { sessionId: string }
-  const rekognition = await getRekognitionClient()
-  const response = await rekognition
-    .getFaceLivenessSessionResults({
-      SessionId: sessionId,
-    })
-    .then((resp) => resp)
+  try {
+    const { sessionId } = req.query as { sessionId: string }
+    const rekognition = await getRekognitionClient()
+    const response = await rekognition
+      .getFaceLivenessSessionResults({
+        SessionId: sessionId,
+      })
+      .then((resp) => resp)
 
-  if (!response) {
-    return reply.status(404).json({
-      error: 'Session not found',
+    if (!response) {
+      return reply.status(404).json({
+        error: 'Session not found',
+      })
+    }
+
+    const isLive = response?.Confidence && response?.Confidence >= 65
+
+    reply.status(200).json({
+      isLive,
+    })
+  } catch (err) {
+    console.log(err)
+    reply.status(500).json({
+      error:
+        'Internal server error! It was not possible to create a liveness session',
     })
   }
-
-  const isLive = response?.Confidence && response?.Confidence >= 65
-
-  reply.status(200).json({
-    isLive,
-  })
 }
